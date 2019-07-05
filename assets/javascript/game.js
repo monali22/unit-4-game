a3e645182021445d626ef77d0d97f6a13865f2ef
Monali Chandurkar <chandurkar.monali123@gmail.com>
2019-07-05 16:20:10 -0700

    updated game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,122 @@
 var wins = 0;
 var losses = 0;
 var check = false;
-
+var sum = 0;
+var randomNumber = 0;
+var nums = [];
+var randomNumberForCrystal = 0;
 $(document).ready(function () {
 
-    function check(randNum) {
-        if (randNum === parseInt($("#score").text())) {
-            wins++;
-            $("#wins").text(wins);
-            $("#wltext").text("You Won");
-            playTheGame();
-            //return;
-        }
-        if (randNum < parseInt($("#score").text())) {
-            losses++;
-            $("#losses").text(losses);
-            $("#wltext").text("You Lost");
-            playTheGame();
-            //return;
-        }
-    }
+    setUp();
+    function setUp() {
+        
+        console.log("In setup");
+        randomNumber = 0;
+        nums = [];
+        randomNumberForCrystal = 0;
 
-    function playTheGame() {
-        var guessNumber = $("#guessNumber");
-
-        var randNum = Math.floor((Math.random() * 100) + 1);
-
-        guessNumber.text(randNum);
+        randomNumber = Math.floor((Math.random() * 100) + 1);
+        console.log(randomNumber);
+        $('#guessNumber').text(randomNumber);
+        $('#score').text(0);
 
 
-        var score = $("#score");
 
-        score.text(0);
-
-
-        $("#wins").text(wins);
-
-        $("#losses").text(losses);
-
-        var nums = [];
-
-
-        for (var i = 1; i < 10; i++) {
-            var randNum2 = Math.floor((Math.random() * 10) + 1);
-            if (nums.indexOf(randNum2) == -1) nums.push(randNum2);
+        while (nums.length != 4) {
+            randomNumberForCrystal = Math.floor((Math.random() * 10) + 1);
+            if (nums.indexOf(randomNumberForCrystal) === -1) {
+                nums.push(randomNumberForCrystal);
+            }
         }
         console.log(nums);
-        if (randNum > parseInt($("#score").text())) {
-        $("#img1").on("click", function () {
-            var num = parseInt($("#score").text()) + nums[0];
-            score.text(num);
-            check(randNum);
-        });
-        $("#img2").on("click", function () {
-            var num = parseInt($("#score").text()) + nums[1];
-            score.text(num);
-            check(randNum);
-        });
-        $("#img3").on("click", function () {
-            var num = parseInt($("#score").text()) + nums[2];
-            score.text(num);
-            check(randNum);
-        });
-        $("#img4").on("click", function () {
-            var num = parseInt($("#score").text()) + nums[3];
-            score.text(num);
-            check(randNum);
-        });
-    }
 
+        $('#img1').attr('value', nums[0]);
+        $('#img2').attr('value', nums[1]);
+        $('#img3').attr('value', nums[2]);
+        $('#img4').attr('value', nums[3]);
+        play();
     }
 
 
+    function play() {
+        
+        $('#img1').unbind().click(function () {
+            console.log("in img1");
+            var num = parseInt($('#score').text());
+            num = num + nums[0];
+            $('#score').text(num);
+            if(parseInt($('#score').text())==randomNumber){
+                console.log("you won");
+                wins++;
+                $('#wins').text(wins);
+                setUp();
+            }
+            else if(parseInt($('#score').text())>randomNumber){
+                console.log("you lost");
+                losses++;
+                $('#losses').text(losses);
+                setUp();
+            }
+        });
 
+        $('#img2').unbind().click(function () {
+            console.log("in img2");
+            var num = parseInt($('#score').text());
+            num = num + nums[1];
+            $('#score').text(num);
+            if(parseInt($('#score').text())==randomNumber){
+                console.log("you won");
+                wins++;
+                $('#wins').text(wins);
+                setUp();
+            }
+            else if(parseInt($('#score').text())>randomNumber){
+                console.log("you lost");
+                losses++;
+                $('#losses').text(losses);
+                setUp();
+            }
+        });
 
-    
+        $('#img3').unbind().click(function () {
+            console.log("in img3");
+            var num = parseInt($('#score').text());
+            num = num + nums[2];
+            $('#score').text(num);
+            if(parseInt($('#score').text())==randomNumber){
+                console.log("you won");
+                wins++;
+                $('#wins').text(wins);
+                setUp();
+            }
+            else if(parseInt($('#score').text())>randomNumber){
+                console.log("you lost");
+                losses++;
+                $('#losses').text(losses);
+                setUp();
+            }
+        });
 
-    playTheGame();
+        $('#img4').unbind().click(function () {
+            console.log("in img4");
+            var num = parseInt($('#score').text());
+            num = num + nums[3];
+            $('#score').text(num);
+            if(parseInt($('#score').text())==randomNumber){
+                console.log("you won");
+                wins++;
+                $('#wins').text(wins);
+                setUp();
+            }
+            else if(parseInt($('#score').text())>randomNumber){
+                console.log("you lost");
+                losses++;
+                $('#losses').text(losses);
+                setUp();
+            }
+        });
 
-    
-
+    }
 });
 
 
